498e1d433009656f22fe53fc9c248a609ae85804
akashgupta200134 <akashgupta_ce_2021@ltce.in>
2024-02-28 18:57:15 +0530

    Initial commit

---
+++ src/Components/data.js
@@ -0,0 +1,75 @@
+const reviews = [
+  {
+    id: 1,
+    Name: "Aiony Haust",
+    job: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D",
+    text: "I've had a fantastic experience working with this company. The team's dedication to excellence is evident in every project they undertake. The company's commitment to staying at the forefront of technology ensures that they deliver innovative solutions. The collaborative atmosphere fosters creativity and teamwork, making it a pleasure to work here.",
+  },
+
+  {
+    id: 2,
+    Name: "jimmy Fermin",
+    job: "Graphic Designer",
+    image:
+      "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    text: "I've had a wonderful experience collaborating with our graphic designer. Their dedication to creative excellence is evident in every design project they tackle. The designer's commitment to staying updated on the latest design trends ensures that they deliver visually appealing and innovative solutions.",
+  },
+
+  {
+    id: 3,
+    Name: "Joseph Gaonzali",
+    job: "Marketing Manager",
+    image:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+    text: "I've had an exceptional experience working with our Marketing Manager. Their dedication to strategic excellence is evident in every campaign they lead. The manager's commitment to staying updated on the latest marketing trends ensures that they deliver impactful and innovative solutions.",
+  },
+
+  {
+    id: 4,
+    Name: "Chinu Sewan",
+    job: "Software Engineer",
+    image:
+      "https://media.istockphoto.com/id/1904130989/photo/portrait-asian-smart-beautiful-businesswoman-wearing-casual-business-clothes-holding-cup-of.jpg?s=1024x1024&w=is&k=20&c=H26z5NKHPnOAS7pdQbfB51oJAlryrXYzdw8x7Ul17_Y=",
+    text: "I've had a fantastic experience working with this company. The team's dedication to excellence is evident in every project they undertake. The company's commitment to staying at the forefront of technology ensures that they deliver innovative solutions. The collaborative atmosphere fosters creativity and teamwork, making it a pleasure to work here.",
+  },
+
+  {
+    id: 5,
+    Name: "Danny Ruso",
+    job: "Content Strategist",
+    image:
+      "https://media.istockphoto.com/id/685132245/photo/mature-businessman-smiling-over-white-background.jpg?s=612x612&w=0&k=20&c=OJk6U-oCZ31F3TGmarAAg2jVli8ZWTagAcF4P-kNIqA=",
+    text: "I've had a fantastic experience collaborating with our Content Writer. Their dedication to writing excellence is evident in every piece of content they produce. The writer's commitment to staying updated on the latest writing styles and industry trends ensures that they deliver engaging and innovative content.",
+  },
+
+  {
+    id: 6,
+    Name: "Raju Rastogi",
+    job: "Data Analyst",
+    image:
+      "https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo=",
+    text: "I've had a fantastic experience working with this company. The team's dedication to excellence is evident in every project they undertake. The company's commitment to staying at the forefront of technology ensures that they deliver innovative solutions. The collaborative atmosphere fosters creativity and teamwork, making it a pleasure to work here.",
+  },
+
+  {
+    id: 7,
+    Name: "Jonny Dalusa",
+    job: "Marketing Manager",
+    image:
+      "https://media.istockphoto.com/id/1326426267/photo/half-length-shot-of-positive-attractive-female-model-with-afro-haircut-feels-good-uses.jpg?s=612x612&w=0&k=20&c=i4IiqGTQleNHBOUcByaCp5QgkxX6jQ_A1X634qB1zzs=",
+    text: "I've had a fantastic experience working with this company. The team's dedication to excellence is evident in every project they undertake. The company's commitment to staying at the forefront of technology ensures that they deliver innovative solutions. The collaborative atmosphere fosters creativity and teamwork, making it a pleasure to work here.",
+  },
+
+  {
+    id: 8,
+    Name: "Shikha Gupta",
+    job: "Product Manager",
+    image:
+      "https://media.istockphoto.com/id/1299077558/photo/lead-yourself-to-a-life-of-success.jpg?s=612x612&w=0&k=20&c=OQZPSnM1Eq-4Xx8bxJE8KQ5olJFfRw_YMc29aQ0Au6U=",
+    text: "I've had a fantastic experience working with this company. The team's dedication to excellence is evident in every project they undertake. The company's commitment to staying at the forefront of technology ensures that they deliver innovative solutions. The ,collaborative atmosphere fosters creativity and teamwork, making it a pleasure to work here.",
+  },
+];
+
+export default reviews;
